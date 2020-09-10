1bb0c16230cde286a869cab2450b77457426f827
Duvanmontes <69734650+Alcalyx12@users.noreply.github.com>
2020-09-09 23:56:02 -0500

    Add files via upload

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float N1, N2, N3, PROMED;
+            String contador;
+
+              Console.WriteLine("DIGITE SU PRIMER NOTA");
+                contador = Console.ReadLine();
+
+
+            N1 = float.Parse(contador);
+              Console.WriteLine("DIGITE SU SEGUNDA NOTA");
+                contador = Console.ReadLine();
+
+            N2 = float.Parse(contador);
+              Console.WriteLine("DIGITE SU TERCER NOTA");
+                contador = Console.ReadLine();
+
+            N3 = float.Parse(contador);
+
+
+            PROMED = (N1+N2+N3)/3;
+
+                if (PROMED< 3)
+              {
+
+              Console.WriteLine("USTED HA REPROBADO CON UN PROMEDIO DE: " + PROMED);
+
+                }
+
+                else if (PROMED>= 3)
+              {
+
+              Console.WriteLine("USTED APROBO CON UN PROMEDIO DE: " + PROMED);
+
+                }
+
+            Console.ReadLine();
+        }
+    }
+}
