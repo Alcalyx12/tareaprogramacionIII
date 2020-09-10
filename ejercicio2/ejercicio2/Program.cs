0d87dfe282ab0dd92ed481cc56bdb241cb045b48
Duvanmontes <69734650+Alcalyx12@users.noreply.github.com>
2020-09-10 00:02:40 -0500

    Add files via upload

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int EDAD;
+            String NOMBR;
+            String SEXO;
+            int NUMPULSAC = 0;
+
+            Console.WriteLine("DIGITE SU NOMBRE");
+             NOMBR = Console.ReadLine();
+
+            Console.WriteLine("DIGITE SU EDAD");
+            EDAD = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("DIGITE SU SEXO F/M ");
+             SEXO = Console.ReadLine();
+
+           
+
+                 if (SEXO == "F")
+
+                {
+                    NUMPULSAC = (220 - EDAD) / 10;
+                 }
+
+                 else if (SEXO == "M")
+                {
+                    NUMPULSAC = (210 - EDAD)/10;
+                 }
+
+
+            Console.WriteLine("LAS PULSACIONES DE " + NOMBR + "  SON  " + NUMPULSAC);
+
+            Console.ReadLine();
+        }
+    }
+}
